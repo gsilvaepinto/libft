0bf4ea0f5848356756028114a58f1f0bc3125a02
gsilvaepinto <109026846+gsilvaepinto@users.noreply.github.com>
2023-05-10 12:32:51 +0100

    Add files via upload

---
+++ ft_strnstr.c
@@ -0,0 +1,39 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strnstr.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: gupinto <gupinto@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/05/10 11:52:40 by gupinto           #+#    #+#             */
+/*   Updated: 2023/05/10 12:22:36 by gupinto          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+char	*ft_strnstr(const char *haystack, const char *needle, size_t len)
+{
+	size_t	i;
+	size_t	j;
+	size_t	needle_len;
+
+	needle_len = ft_strlen(needle);
+	if (needle_len == 0)
+		return ((char *)haystack);
+	i = 0;
+	while (i < len && haystack[i])
+	{
+		if (haystack[i] == needle[0])
+		{
+			j = 1;
+			while (j < needle_len && haystack[i + j] == needle[j] && i
+				+ j < len)
+				j++;
+			if (j == needle_len)
+				return ((char *)(haystack + i));
+		}
+		i++;
+	}
+	return (NULL);
+}
